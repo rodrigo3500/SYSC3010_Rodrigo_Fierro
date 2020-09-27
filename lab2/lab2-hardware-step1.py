9b24b7b4f316d38f4d368e306f1fac7e706ea604
rodrigo_3500 <rodrigo_3500@live.com>
2020-09-27 13:55:59 -0400

    Adding masterpy code for lab 1 from section 3.2

--- lab2/lab2-hardware-step1.py
+++ lab2/lab2-hardware-step1.py
@@ -4,7 +4,6 @@ import time
 s = SenseHat()
 s.low_light = True
 
-
 g = (0, 255, 0)
 y = (255, 255, 0)
 b = (0, 0, 255)
@@ -14,81 +13,38 @@ o = (0,0,0)
 p = (255,105, 180)
 
 
-def trinket_logo():
+def letter_r():
     logo = [
     o, o, o, o, o, o, o, o,
-    o, y, y, y, b, g, o, o,
-    y, y, y, y, y, b, g, o,
-    y, y, y, y, y, b, g, o,
-    y, y, y, y, y, b, g, o,
-    y, y, y, y, y, b, g, o,
-    o, y, y, y, b, g, o, o,
-    o, o, o, o, o, o, o, o,
+    o, r, r, r, o, o, o, o,
+    o, r, o, o, r, o, o, o,
+    o, r, o, o, r, o, o, o,
+    o, r, r, r, o, o, o, o,
+    o, r, r, o, o, o, o, o,
+    o, r, o, r, o, o, o, o,
+    o, r, o, o, r, o, o, o,
     ]
     return logo
 
 
-def raspi_logo():
+def letter_f():
     logo = [
-    o, g, g, o, o, g, g, o,
-    o, o, g, g, g, g, o, o,
-    o, o, r, r, r, r, o, o,
-    o, r, r, r, r, r, r, o,
-    r, r, r, r, r, r, r, r,
-    r, r, r, r, r, r, r, r,
-    o, r, r, r, r, r, r, o,
-    o, o, r, r, r, r, o, o,
+    o, o, o, o, o, o, o, o,
+    o, b, b, b, b, o, o, o,
+    o, b, o, o, o, o, o, o,
+    o, b, o, o, o, o, o, o,
+    o, b, b, b, b, o, o, o,
+    o, b, o, o, o, o, o, o,
+    o, b, o, o, o, o, o, o,
+    o, b, o, o, o, o, o, o,
     ]
     return logo
 
-
-def plus():
-    logo = [
-    o, o, o, o, o, o, o, o,
-    o, o, o, w, w, o, o, o,
-    o, o, o, w, w, o, o, o,
-    o, w, w, w, w, w, w, o,
-    o, w, w, w, w, w, w, o,
-    o, o, o, w, w, o, o, o,
-    o, o, o, w, w, o, o, o,
-    o, o, o, o, o, o, o, o,
-    ]
-    return logo
-
-
-def equals():
-    logo = [
-    o, o, o, o, o, o, o, o,
-    o, w, w, w, w, w, w, o,
-    o, w, w, w, w, w, w, o,
-    o, o, o, o, o, o, o, o,
-    o, o, o, o, o, o, o, o,
-    o, w, w, w, w, w, w, o,
-    o, w, w, w, w, w, w, o,
-    o, o, o, o, o, o, o, o,
-    ]
-    return logo
-
-
-def heart():
-    logo = [
-    o, o, o, o, o, o, o, o,
-    o, p, p, o, p, p, o, o,
-    p, p, p, p, p, p, p, o,
-    p, p, p, p, p, p, p, o,
-    o, p, p, p, p, p, o, o,
-    o, o, p, p, p, o, o, o,
-    o, o, o, p, o, o, o, o,
-    o, o, o, o, o, o, o, o,
-    ]
-    return logo
-
-
-images = [trinket_logo, trinket_logo, plus, raspi_logo, raspi_logo, equals, heart, heart]
-count = 0
 
 while True:
-    s.set_pixels(images[count % len(images)]())
     time.sleep(.75)
-    count += 1
+    s.set_pixels(letter_r())
+    time.sleep(.75)
+    s.set_pixels(letter_f())
+
 
